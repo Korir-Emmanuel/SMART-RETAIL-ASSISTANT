cd455e2e444a6289b9a031baac12427d1d8096ce
Evans Makau <makauevans01@gmail.com>
2025-06-01 17:31:22 +0300

    added stopwords

--- utils/supplier_finder.py
+++ utils/supplier_finder.py
@@ -1,6 +1,15 @@
 import pandas as pd
+import re
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+
+def clean_text(text):
+
+    stopwords = ENGLISH_STOP_WORDS
+    words = re.findall(r'\b\w+\b', text.lower())
+    return [word for word in words if word not in stopwords]
+
 def find_suppliers(text, df):
-    user_words = text.lower().split()
+    user_words = clean_text(text)
 
     def match_row(desc):
         desc = str(desc).lower()
